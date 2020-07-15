c5235533d7e5e338e665f2fad1527d1083f0a087
Rahaf Aljerwi <aljerwi.rahaf@yahoo.com>
2020-07-15 15:11:31 -0500

    added two view controllers and bottom nav bar

---
+++ Final Project/HomePageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageViewController.swift
+//  Final Project
+//
+//  Created by Rahaf Aljerwi on 7/15/20.
+//  Copyright © 2020 Rahaf Aljerwi. All rights reserved.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+
+    
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Final Project/HomeUIBarButtonItem.swift
@@ -0,0 +1,13 @@
+//
+//  HomeUIBarButtonItem.swift
+//  Final Project
+//
+//  Created by Rahaf Aljerwi on 7/15/20.
+//  Copyright © 2020 Rahaf Aljerwi. All rights reserved.
+//
+
+import UIKit
+
+class HomeUIBarButtonItem: UIBarButtonItem {
+
+}
