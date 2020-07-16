56fc5581aeb6fe4c394bc822e6957ad4b4e3c1ff
Rahaf Aljerwi <aljerwi.rahaf@yahoo.com>
2020-07-15 19:15:10 -0500

    fixed buttons and added more view controllers

--- Final Project/HomePageViewController.swift
+++ Final Project/HomePageViewController.swift
@@ -17,10 +17,16 @@ class HomePageViewController: UIViewController {
 
     
     }
+
+    @IBAction func HomeButton(_ sender: UIButton) {
+
+    }
+    
     /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+   
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
